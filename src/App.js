7b02fce535dec9c53357e22513535640ccc2d3af
TJ-2004 <124814940+TJ-2004@users.noreply.github.com>
2024-03-09 16:29:41 +0530

    Setup Api Call

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
-
+import { AppContext } from "./context/contextApi";
 const App = () => {
   return (
-    <div>
-      <h1 className="text-4xl">Hello</h1>
-    </div>
+    <AppContext>
+      <div>
+        <h1 className="text-4xl">Hello</h1>
+      </div>
+    </AppContext>
   );
 };
 
---
+++ src/context/contextApi.js
@@ -0,0 +1,44 @@
+import React, { createContext, useState, useEffect } from "react";
+
+import { fetchdatafromapi } from "../utils/api";
+
+export const Context = createContext();
+
+export const AppContext = (props) => {
+  const [loading, setLoading] = useState(false);
+  const [searchResult, setSearchResult] = useState(false);
+  const [selectCategories, setSelectCategories] = useState("New");
+  const [mobileMenu, setMobileMenu] = useState(false);
+
+  useEffect(() => {
+    fetchSelectedCategoryData(selectCategories);
+  }, [selectCategories]);
+
+
+  const fetchSelectedCategoryData = (query) => {
+    setLoading(true);
+    fetchdatafromapi(`search/?q=${query}`).then((response) => {
+      console.log(response);
+      //   setSearchResult(response);
+      setLoading(false);
+    });
+  };
+
+
+  return (
+    <Context.Provider
+      value={{
+        loading,
+        setLoading,
+        searchResult,
+        // setSearchResult,
+        selectCategories,
+        setSelectCategories,
+        mobileMenu,
+        setMobileMenu,
+      }}
+    >
+      {props.children}
+    </Context.Provider>
+  );
+};
